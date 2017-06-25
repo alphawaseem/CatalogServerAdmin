e6f2aeb43eabc30d3a679e51876e5220e328824b
Waseem Ahmed <alphawaseem@gmail.com>
2017-06-25 15:55:15 +0530

    working

--- __init__.py
+++ __init__.py
@@ -281,7 +281,7 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secret.json', scope='')
+        oauth_flow = flow_from_clientsecrets('/var/www/catalog/catalog/client_secret.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code).to_json()
     except FlowExchangeError:
